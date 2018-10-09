7880ef9f7bea56b17e2afafe675295997544fba2
Ethan Lengfeld <elengfeld@wisc.edu>
2018-10-09 12:32:34 -0500

    Added all needed files with file headers

---
+++ main.c
@@ -0,0 +1,37 @@
+////////////////////////////////////////////////////////////////////////////////
+// Main File: main.c
+// This File: main.c
+// Other Files: main.c / quote.c / reader.c / munch1.c / munch2.c / writer.c / quote.h / reader.h / munch1.h / munch2.h / writer.h / makefile
+// Semester:         CS 537 Fall 2018
+//
+// Author:           Ethan Lengfeld / William Pechous
+// Email:            elengfeld@wisc.edu / wpechous@wisc.edu
+// CS Login:         lengfeld / pechous
+//
+/////////////////////////// OTHER SOURCES OF HELP //////////////////////////////
+//                   fully acknowledge and credit all sources of help,
+//                   other than Instructors and TAs.
+//
+// Persons:          None
+//
+//
+// Online sources:   None
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "quote.h";
+#include "reader.h";
+#include "munch1.h";
+#include "munch2.h";
+#include "writer.h";
+
+int main(int argc, char *argv[]){
+
+	// create queue
+	
+	// lock the threads
+
+
+
+}
---
+++ reader.h
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////////////////////////////////////
+// Main File: main.c
+// This File: reader.h
+// Other Files: main.c / quote.c / reader.c / munch1.c / munch2.c / writer.c / quote.h / reader.h / munch1.h / munch2.h / writer.h / makefile
+// Semester:         CS 537 Fall 2018
+//
+// Author:           Ethan Lengfeld / William Pechous
+// Email:            elengfeld@wisc.edu / wpechous@wisc.edu
+// CS Login:         lengfeld / pechous
+//
+/////////////////////////// OTHER SOURCES OF HELP //////////////////////////////
+//                   fully acknowledge and credit all sources of help,
+//                   other than Instructors and TAs.
+//
+// Persons:          None
+//
+//
+// Online sources:   None
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef READER
+#define READER
+
+
+#endif
